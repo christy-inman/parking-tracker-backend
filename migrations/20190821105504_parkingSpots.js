2f20bc7c8644a13509035300b330c5370facea6c
github username <github email address>
2019-08-21 11:21:36 -0600

    fixed migration, working on seeds

--- migrations/20190821105504_parkingSpots.js
+++ migrations/20190821105504_parkingSpots.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('parking_spots', (spot) => {
       spot.increments('id')
-      spot.string('cross streets', 50)
+      spot.string('cross_streets', 50)
       spot.date('date')
       spot.string('notes')
   })
